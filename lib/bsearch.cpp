b776fcaa25604c864025e05d199ea247c4540846
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-12-04 12:07:41 -0700

    Add working node class

--- lib/bsearch.cpp
+++ lib/bsearch.cpp
@@ -7,3 +7,14 @@ class Node
 				int left;
 				int right;
 };
+
+int main () 
+{ 
+	Node node1;
+	node1.key = 5;
+	node1.left = 3;
+	node1.right= 3;
+	std::cout << node1.key << " " << node1.left << " " << node1.right; 
+	std::cout << "Hello World!"; 
+	return 0;
+}
