175a7c170274f5910add047ff0e3b57c897eb7fb
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-12-05 14:59:03 -0700

    Add stopping point before pivot

--- lib/bsearch.cpp
+++ lib/bsearch.cpp
@@ -8,13 +8,33 @@ class Node
 				int right;
 };
 
+class Tree
+{
+	public:
+	int root;
+	void readKey(int key);
+
+};
+
+void Tree::readKey(int key) {  // Method/function defined inside the class
+	if ( root == 0 ) { 
+		//set the root
+		root == key;
+	}
+}
+
+
 int main () 
 { 
 	Node node1;
+	Tree tree1;
+	tree1.root = 0;
+	tree1.readKey(10);
 	node1.key = 5;
 	node1.left = 3;
 	node1.right= 3;
+	
 	std::cout << node1.key << " " << node1.left << " " << node1.right; 
-	std::cout << "Hello World!"; 
+	std::cout << tree1.root; 
 	return 0;
 }
